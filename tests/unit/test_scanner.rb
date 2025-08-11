2ea97a23e1f8724991c00b9d9c9f8987602d3de5
3rIcF <97833428+3rIcF@users.noreply.github.com>
2025-08-11 04:10:38 +0200

    Add build and smoke scripts

--- tests/unit/test_scanner.rb
+++ tests/unit/test_scanner.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require_relative '../../ElementaroInfo/lib/scanner'
 
+# Unit tests for the Scanner placeholder class
 class TestScanner < Minitest::Test
   def test_can_create_scanner_instance
     scanner = ElementaroInfo::Scanner.new
---
+++ tools/smoke.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'open3'
+
+CHECKS = [
+  ['rubocop', 'rubocop'],
+  ['tests',   'ruby -Itests tests/unit/test_scanner.rb']
+].freeze
+
+CHECKS.each do |name, cmd|
+  puts "==> #{name}: #{cmd}"
+  system(cmd) || abort("#{name} failed")
+end
+
+puts 'All smoke checks passed.'
