6fa180591c549b3ded77f905a5efa7941ee1f4ab
3rIcF <97833428+3rIcF@users.noreply.github.com>
2025-08-11 05:23:19 +0200

    Capture timer id for thumbnail queue

---
+++ tests/stubs/sketchup.rb
@@ -0,0 +1,54 @@
+module Sketchup
+  def self.temp_dir
+    Dir.pwd
+  end
+
+  def self.active_model
+    Model.new
+  end
+
+  class Model
+    def definitions
+      {}
+    end
+
+    def selection
+      []
+    end
+
+    def entities
+      []
+    end
+
+    def layers
+      []
+    end
+
+    def add_observer(*); end
+  end
+
+  class ModelObserver; end
+  class SelectionObserver; end
+end
+
+module UI
+  def self.menu(_name)
+    Menu.new
+  end
+
+  class Menu
+    def add_submenu(_name)
+      self
+    end
+
+    def add_item(_name)
+      self
+    end
+  end
+
+  def self.start_timer(*args)
+    1
+  end
+
+  def self.stop_timer(_id); end
+end
