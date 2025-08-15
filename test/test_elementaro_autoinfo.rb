155633a02e45824953c02d50d32cd020c1f198e4
3rIcF <97833428+3rIcF@users.noreply.github.com>
2025-08-16 01:57:53 +0200

    ci(test): align workflow with renamed test and Ruby 3.2

--- test/test_elementaro_autoinfo.rb
+++ test/test_elementaro_autoinfo.rb
@@ -1,15 +1,20 @@
+# frozen_string_literal: true
+
 require 'minitest/autorun'
-$LOAD_PATH.unshift File.expand_path('stubs', __dir__)
-require 'sketchup'
-require 'extensions'
+require_relative 'stubs/sketchup'
+require_relative 'stubs/extensions'
 
 class ElementaroAutoinfoDevTest < Minitest::Test
+  STUB_DIR = File.expand_path('stubs', __dir__)
+
   def setup
     Sketchup.clear_extensions
   end
 
   def test_extension_registration
+    $LOAD_PATH.unshift STUB_DIR
     load File.expand_path('../elementaro_autoinfo_dev.rb', __dir__)
+    $LOAD_PATH.delete(STUB_DIR)
     assert_equal 1, Sketchup.extensions.length
     ext = Sketchup.extensions.first
     assert_equal 'Elementaro AutoInfo Dev', ext.name
